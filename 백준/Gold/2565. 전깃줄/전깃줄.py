0b57776a8158d9cec251e8561d7d878e43342233
Jeong Jun Seo <78674565+sunseo18@users.noreply.github.com>
2022-04-12 21:09:40 +0900

    [Gold V] Title: 전깃줄, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+lineList = []
+
+for _ in range(N):
+    lineList.append(list(map(int, input().split())))
+
+lineList.sort()
+
+dp = [1]*N
+for i in range(N):
+    for j in range(i):
+        if lineList[i][1] > lineList[j][1] and dp[i] < dp[j]+1:
+            dp[i] = dp[j] + 1
+
+print(N-max(dp))
