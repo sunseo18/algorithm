f4bc45227bf594fb1c7abe908b7f0e54d28a752d
Jeong Jun Seo <78674565+sunseo18@users.noreply.github.com>
2022-05-16 23:04:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 808 ms, Memory: 40376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,10 +2,13 @@ import sys
 read = sys.stdin.readline
 
 N, K = map(int, read().split())
-cache = [0] * (K+1)
+cache = {0: 0}
 
 for _ in range(N):
-    w, v = map(int, read().split())
-    for j in range(K, w-1, -1):
-        cache[j] = max(cache[j], cache[j-w] + v)
-print(cache[-1])
+    curr_w, curr_v = map(int, read().split())
+    temp = {}
+    for w, v in cache.items():
+        if curr_w + w <= K and curr_v + v > cache.get(curr_w + w, 0):
+            temp[curr_w + w] = curr_v + v
+    cache.update(temp)
+print(max(cache.values()))
