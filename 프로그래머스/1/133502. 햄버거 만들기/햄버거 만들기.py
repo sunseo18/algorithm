a3b0fb1f2d4dfc2a24776f3d263a81e5d42b9b4e
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2023-11-09 02:13:25 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -8,12 +8,11 @@ def solution(ingredient):
         j += 1
         stack.append(i)      
         
-        if j > 3:
-            if stack[-4:] == [1, 2, 3, 1]:
-                answer += 1
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                stack.pop()
+        if stack[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.pop()
         
     return answer
