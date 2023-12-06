03dcd880566c1c9150fafa53e08238df1d059cc6
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2023-12-06 12:36:07 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,15 @@
+def solution(data, ext, val_ext, sort_by):
+    ext_dict = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
+    
+    semi_answer = []
+
+    # val_ext보다 더 작은 값들 검색
+    for d in data:
+        if d[ext_dict[ext]] < val_ext:
+            semi_answer.append(d)
+    
+
+    # 정렬
+    semi_answer.sort( key = lambda k : k[ext_dict[sort_by]])
+
+    return semi_answer
