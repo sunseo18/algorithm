1d117c3387f8cbbfb1a01722feb086954d63da7d
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2023-12-16 15:50:22 +0900

    [level 2] Title: 배달, Time: 0.68 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 27.64 ms
+메모리: 10.1 MB, 시간: 0.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 15:1:46
+2023년 12월 6일 15:50:20
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,23 +1,45 @@
 MAX = 500001
+
+def get_smallest_node(d, visited):
+    min = MAX
+    min_idx = 0
+    for i in range(2, len(d)):
+        if not visited[i]:
+            if d[i] < min:
+                min = d[i]
+                min_idx = i
+    return min_idx
+            
+    
 def solution(N, road, K):
     answer = 0
     
     w = [[MAX for _ in range(N+1)] for _ in range(N+1)]
+    visited = [False, False] + ([False] * (N-1))
     for r in road:
         if r[2] < w[r[0]][r[1]]:
             w[r[0]][r[1]] = r[2]
             w[r[1]][r[0]] = r[2]
-            
-    for i in range(N + 1):
-        w[i][i] = 0
-        
-    for path in range(N + 1):
-        for i in range(N + 1):
-            for j in range(N + 1):
-                if w[i][j] > w[i][path] + w[path][j]:
-	                    w[i][j] = w[i][path] + w[path][j]
-        
-    for l in w[1]:
+    
+    for i in range(N+1):
+        for j in range(N+1):
+            if i == j:
+                w[i][j] = 0
+                
+    d = w[1]
+
+    visited[1] = True
+    last = 1
+    for _ in range(2, N+1):
+        for i in range(2, N+1):
+            if not visited[i]:
+                d[i] = min(d[i], d[last] + w[last][i])
+                
+        last = get_smallest_node(d, visited)
+        visited[last] = True
+
+         
+    for l in d:
         if l <= K:
             answer += 1
             
