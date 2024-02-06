95fe65a9b02005b7182427f148614d640466c3de
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-02-07 01:23:23 +0900

    [level 2] Title: 전화번호 목록, Time: 108.19 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,8 @@
-def solution(phone_book): 
+def solution(phoneBook):
+    phoneBook.sort()
 
-    # 1.Hash map생성
-    hash_map = {} 
-    for nums in phone_book: 
-        hash_map[nums] = 1 
-    
-    # 2.접두어가 Hash map에 존재하는지 찾기 
-    for nums in phone_book: 
-        arr = "" 
-        for num in nums: 
-            arr += num
-    
-            # 3. 본인 자체일 경우는 제외
-            if arr in hash_map and arr != nums:       
-                return False 
-                
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        
+        if p2.startswith(p1):
+            return False
     return True
