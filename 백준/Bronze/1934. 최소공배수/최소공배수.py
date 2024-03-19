76c24bfa4e7be74e5f1959d4bc129540df59fc1c
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-03-19 22:56:53 +0900

    [Bronze I] Title: 최소공배수, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -5,11 +5,9 @@ for _ in range(T):
     numbers.append(map(int, input().split()))
 
 def gcd(a, b):
-    while b != 0:
-        r = a % b
-        a = b
-        b = r
-    return a
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
         
 for i in range(T):
     A, B = numbers[i]
