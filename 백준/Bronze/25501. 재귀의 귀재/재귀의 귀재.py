06f6db3f4c15e1e8a0b3e3c75a6a14bdade82dea
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-03-28 23:34:42 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 464 ms, Memory: 31180 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,21 @@
+import sys
+
+def recursion(s, l, r):
+    global count
+    count += 1
+    if l >= r:
+        return 1
+    if s[l] != s[r]:
+        return 0
+    else:
+        return recursion(s, l+1, r-1)
+
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s) -1)
+
+n = int(input())
+
+for _ in range(n):
+    count = 0
+    print(isPalindrome(sys.stdin.readline().strip()), count)
