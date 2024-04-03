07288b130e601989f735308e717a8910a0dc8485
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-04-04 00:27:28 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 256 ms, Memory: 116764 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -11,7 +11,11 @@ def w(a, b, c):
     if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
 
-    result = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    if a < b and b < c:
+        result = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+
+    else:
+        result = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
     d[(a,b,c)] = result
     return result
 
