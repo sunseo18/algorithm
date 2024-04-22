550afc3f707d028f5908c07555036327d63a50d6
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-04-22 21:31:57 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 116200 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,26 +1,12 @@
 import sys
-from collections import defaultdict
+from itertools import permutations
 
 n, m = map(int, input().split())
 
-dic = defaultdict(bool)
+arr = [str(i) for i in range(1, n+1)]
 
-numbers = [ i for i in range(1, n+1) ]
-def dfs(tmp, depth, dic):
-    if depth == len(tmp):
-        for t in tmp:
-            sys.stdout.write(str(t))
-            sys.stdout.write(' ')
-        sys.stdout.write("\n")
-        return
-    for n in numbers:
-        if dic[n]:
-            continue
-        tmp[depth] = n
-        dic[n] = True
-        
-        dfs(tmp, depth + 1, dic)
-        tmp[depth] = 0
-        dic[n] = False
+permuList = list(permutations(arr, m))
 
-dfs([0]*m, 0, dic)
+for p in permuList:
+    sys.stdout.write(" ".join(p))
+    sys.stdout.write("\n")
