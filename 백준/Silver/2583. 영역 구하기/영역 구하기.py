4af8d27cf356eb96feaa75d556b6289356981030
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-04-24 22:48:59 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 32428 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,49 @@
+import sys
+
+sys.setrecursionlimit(10**7)
+
+m, n, k = map(int, sys.stdin.readline().strip().split())
+
+array = [[0 for _ in range(m)] for _ in range(n)]
+
+
+for i in range(k):
+    leftX, leftY, rightX, rightY = map(int, sys.stdin.readline().strip().split())
+
+    for x in range(leftX, rightX):
+        for y in range(leftY, rightY):
+            array[x][y] = 1
+
+def dfs(i, j, depth):
+    global result
+    if not ((n > i >= 0) and (m > j >= 0)):
+        return
+    
+    if array[i][j] != 0:
+        return
+    
+    else:
+        array[i][j] = 1
+        depth += 1
+        result += 1
+
+        dfs(i-1, j, depth)
+        dfs(i+1, j, depth)
+        dfs(i, j-1, depth)
+        dfs(i, j+1, depth)
+
+answersLength = 0
+answers = []
+for i in range(n):
+    for j in range(m):
+        if array[i][j] != 1:
+            result = 0
+            dfs(i, j, 0)
+            if result != 0:
+                answersLength += 1
+                answers.append(result)
+
+print(answersLength)
+for a in sorted(answers):
+    print(a, end= " ")
+    
