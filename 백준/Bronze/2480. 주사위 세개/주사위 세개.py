e329e122512fb175c19be52c1c4e8da3491b192d
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-05-14 23:41:15 +0900

    [Bronze IV] Title: 주사위 세개, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,20 @@
+import sys
+
+a, b, c = map(int, sys.stdin.readline().strip().split())
+
+if a == b and b == c:
+    print(10000 + (a * 1000))
+elif (a == b and b != c):
+    print(1000 + (a * 100))
+
+elif a == c and a != b:
+    print(1000 + (a * 100))
+
+elif (b == c and a!=b):
+    print(1000 + (b * 100))
+
+elif a != b and b != c:
+    print( max(a,b,c)*100)
+
+
+                
