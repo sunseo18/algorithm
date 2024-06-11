43d565b0301b63f4d5b7a5e7c00a64cb4b302886
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-06-11 23:19:59 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 31596 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,40 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+
+sys.setrecursionlimit(9999999)
+answer = 0
+di = [1, -1, 0, 0]
+dj = [0, 0, 1, -1]
+
+def dfs(i, j, maps, m, n):
+    if maps[i][j] == 0:
+        return
+
+    maps[i][j] = 0
+    
+    for d in range(4):
+        ni, nj = i + di[d], j + dj[d]
+
+        if 0 <= ni < n and 0 <= nj < m:
+            dfs(ni, nj, maps, m, n)
+            
+    
+for _ in range(t):
+    m, n, k = map(int, sys.stdin.readline().strip().split())
+
+    maps = [[0 for _ in range(m)] for _ in range(n)]
+
+    for _ in range(k):
+        j, i = map(int, sys.stdin.readline().strip().split())
+
+        maps[i][j] = 1
+
+    answer = 0
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] == 1:
+                answer += 1
+                dfs(i, j, maps, m, n)
+
+    print(answer)
