728d5fdc42ef9f2d8d660660b88c138d5c61eddb
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-06-27 22:59:42 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 31484 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,69 +1,55 @@
 import sys
-from collections import deque
-
+sys.setrecursionlimit(10 ** 6)
 n = int(sys.stdin.readline().strip())
 
 array = []
 for _ in range(n):
     array.append(sys.stdin.readline().strip())
 
-visited_no = [[False for _ in range(n)] for _ in range(n)]
-visited_yes = [[False for _ in range(n)] for _ in range(n)]
-
 di = [0, 0, 1, -1]
 dj = [1, -1, 0, 0]
 
-def bfs(i, j, s):
-    queue = deque([(i, j, s)])
+def dfs_no(i, j, s):
+    for d in range(4):
+        ni, nj = i + di[d], j + dj[d]
 
-    while queue:
-        ci, cj, s = queue.popleft()
+        if 0 <= ni < n and 0 <= nj < n and not visited_no[ni][nj] and array[ni][nj] == s:
+            visited_no[ni][nj] = True
+            dfs_no(ni, nj, s)
 
-        for d in range(4):
-            ni, nj = ci+di[d], cj+dj[d]
+def dfs_yes(i, j, s):
+    for d in range(4):
+        ni, nj =i + di[d], j + dj[d]
 
-            if 0 <= ni < n and 0 <= nj < n and not visited_no[ni][nj] and array[ni][nj] == s:
-                visited_no[ni][nj] = True
-                queue.append((ni, nj, s))
-
-def bfs_yes(i, j, s):
-    queue = deque([(i, j, s)])
-
-    while queue:
-        ci, cj, s = queue.popleft()
-
-
-        for d in range(4):
-            ni, nj = ci+di[d], cj+dj[d]
-
-            if 0 <= ni <n and 0 <= nj < n and not visited_yes[ni][nj]:
-                if array[i][j] == 'R' or array[i][j] == 'G':
-                    if array[ni][nj] == 'B':
-                        continue
-                    else:
-                        visited_yes[ni][nj] = True
-                        queue.append((ni, nj, array[ni][nj]))
-
+        if 0 <= ni < n and 0 <= nj < n and not visited_yes[ni][nj]:
+            if s == 'R' or s == 'G':
+                if array[ni][nj] == 'B':
+                    continue
                 else:
-                    if array[ni][nj] == s:
-                        visited_yes[ni][nj] = True
-                        queue.append((ni, nj, s))
-    
+                    visited_yes[ni][nj] = True
+                    dfs_yes(ni, nj, array[ni][nj])
+            else:
+                if array[ni][nj] == s:
+                    visited_yes[ni][nj] = True
+                    dfs_yes(ni, nj, s)
                     
 answer_no = 0
-answer_yes = 0
-
+visited_no = [[False for _ in range(n)] for _ in range(n)]
 for i in range(n):
     for j in range(n):
         if not visited_no[i][j]:
             visited_no[i][j] = True
-            bfs(i, j, array[i][j])
+            dfs_no(i, j, array[i][j])
             answer_no += 1
+
+answer_yes = 0
+visited_yes = [[False for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    for j in range(n):
         if not visited_yes[i][j]:
             visited_yes[i][j] = True
-            bfs_yes(i, j, array[i][j])
-            answer_yes+=1
-
+            dfs_yes(i, j, array[i][j])
+            answer_yes += 1
     
 
 sys.stdout.write(str(answer_no) + ' '+ str(answer_yes))
