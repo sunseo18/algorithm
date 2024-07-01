a531359cb111c1c807c978996fe33d745f394465
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2024-07-02 00:10:43 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,45 +1,43 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
- 
-n, m = map(int, input().split()) # 사다리 수, 뱀 수
- 
-board = [0] * 101
+
+n, m = map(int, sys.stdin.readline().split())
+
 visited = [False] * 101
- 
+
 ladder = dict()
+snake = dict()
+
 for _ in range(n):
-    i,j = map(int,input().split())
-    ladder[i] = j
- 
-snack = dict()
+    start, end = map(int, sys.stdin.readline().strip().split())
+    ladder[start]= end
 for _ in range(m):
-    i,j = map(int,input().split())
-    snack[i] = j
- 
-def bfs(start):
-    q = deque()
-    q.append(start)
- 
-    visited[start] = True
- 
-    while q:
-        cur = q.popleft()
- 
-        for i in range(1, 7): # 주사위 1 ~ 6
-            next = cur + i
- 
-            if 0 < next <= 100 and not visited[next]:
-                if next in ladder:
-                    next = ladder[next]
-                
-                if next in snack:
-                    next = snack[next]
-                
-                if not visited[next]:
-                    q.append(next)
-                    visited[next] = True
-                    board[next] = board[cur] + 1
- 
-bfs(1) # 1부터 시작
-print(board[100])
+    start, end = map(int, sys.stdin.readline().strip().split())
+    snake[start] = end
+
+
+queue = deque([(1, 0)])
+
+while queue:
+    cur, depth = queue.popleft()
+    
+    if cur == 100:
+        print(depth)
+        sys.exit()
+
+    for i in range(1, 7):
+        next = cur + i
+
+        if 0 <= next <= 100 and not visited[next]:
+
+            if next in ladder.keys():
+                next = ladder[next]
+            if next in snake.keys():
+                next = snake[next]
+
+            if not visited[next]:
+                visited[next] = True
+                queue.append((next, depth + 1))
+
+
+
