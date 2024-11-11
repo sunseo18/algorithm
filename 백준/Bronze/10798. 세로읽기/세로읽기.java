c4a7ce9631b85fd22463d33c55c528e0592996c1
JunSeo <78674565+sunseo18@users.noreply.github.com>
2024-11-12 00:05:59 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,32 +1,63 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		char[][] c_arr = new char[5][15];
-		int i,j;
-		int max = 0;
-		
-		for(i=0;i<5;i++) {
-			String str = sc.next();
-			
-			if(str.length()>max) 
-				max = str.length();
-			
-			for(j=0;j<str.length();j++) {
-				c_arr[i][j] = str.charAt(j);
-			}
-		}
-		
-		
-		for(i=0;i<max;i++) {
-			for(j=0;j<5;j++) {
-				if(c_arr[j][i] == '\0')
-					continue;
-				System.out.print(c_arr[j][i]);
-			}
-		}
-		sc.close();
-	}
+
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int height = 5;
+        final int width = 15;
+        char[][] chars = new char[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                chars[i][j] = ' ';
+            }
+        }
+
+        for (int i = 0; i < height; i++) {
+            String line = br.readLine();
+
+            for (int j = 0; j < line.length(); j++) {
+                chars[i][j] = line.charAt(j);
+            }
+        }
+
+
+        for (int j = 0; j < width; j++) {
+            for (int i = 0; i < height; i++) {
+                if (chars[i][j] != ' ') {
+                    System.out.print(chars[i][j]);
+                }
+            }
+        }
+
+    }
+
+    public static class Node {
+        private int data;
+        private List<Node> child;
+
+        public Node(int data) {
+            this.data = data;
+            this.child = null;
+        }
+
+        public void addChild(Node child) {
+            this.child.add(child);
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public List<Node> getChild() {
+            return child;
+        }
+    }
 }
