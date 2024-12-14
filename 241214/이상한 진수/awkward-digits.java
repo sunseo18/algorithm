d1bdfdd39706c3be1414b6ac0ed43aba161efe56
JunSeo <78674565+sunseo18@users.noreply.github.com>
2024-12-14 23:23:21 +0900

    Title: 이상한 진수, Time: 869ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241214/이상한 진수/awkward-digits.java
+++ 241214/이상한 진수/awkward-digits.java
@@ -1,5 +1,6 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
