114751a301ce49d3f96f9a127a15381bb389b51c
JunSeo <78674565+sunseo18@users.noreply.github.com>
2024-12-14 23:22:35 +0900

    Title: 이상한 진수, Time: 869ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241214/이상한 진수/awkward-digits.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException
+    {
+
+        String binary = br.readLine();
+        String thirdary = br.readLine();
+
+
+        int min = (int) Math.pow(2, (binary.length()-1));
+        int max = (int) Math.pow(2, binary.length())  - 1;
+
+        for (int number = min; number < max+1; number++) {
+            String tmp =  Integer.toString(number,3);
+            if (check(tmp, thirdary)) {
+                System.out.println(Integer.parseInt(tmp, 3));
+            }
+        }
+
+    }
+
+    public static boolean check(String a, String b) {
+        int diff = 0;
+
+        if (a.length() != b.length() )
+            return false;
+
+        for ( int i = 0; i < a.length(); i++) {
+            if ( a.charAt(i) != b.charAt(i)) diff++;
+            if (diff > 1) 
+                return false;
+        }
+
+        if (( a.length() == b.length() ) && diff == 1)
+            return true;
+        return false;
+    }
+}
