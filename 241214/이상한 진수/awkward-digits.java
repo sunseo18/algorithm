eb1fc865e7ca13fa1e247ab6cecf72d0674f537d
JunSeo <78674565+sunseo18@users.noreply.github.com>
2024-12-14 23:28:10 +0900

    Title: 이상한 진수, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241214/이상한 진수/awkward-digits.java
+++ 241214/이상한 진수/awkward-digits.java
@@ -1,29 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        int[] arr = new int[a.length()]; // 10진수를 저장할 배열
-        int res = Integer.MIN_VALUE; 
+        String b = sc.next();
 
-        for(int i = 0; i < a.length(); i++){
-            char c = a.charAt(i);
-            if(c == '1')
-                c = '0';
-            else if(c == '0')
-                c = '1';
+        List<Integer> binaryList = new ArrayList<>();
+        List<Integer> ternaryList = new ArrayList<>();
 
-			// 비트 반전시킨 이진수 문자열 binary 변수에 저장
-            String binary = a.substring(0, i) + c + a.substring(i+1); 
-            
-            // 2진수 → 10진수로 변환하여 배열에 저장
-            arr[i] = Integer.parseInt(binary, 2);
+        // 2진수를 1개씩 변경하여 10진수로 변환하여 리스트에 담기
+        for (int i = 0; i < a.length(); i++) {
+            char current = a.charAt(i);
+            String front = a.substring(0, i);
+            String back = a.substring(i + 1);
+
+            if (current == '0') {
+                current = '1';
+            } else {
+                current = '0';
+            }
+
+            int decimal = binaryToDecimal(front + current + back);
+            binaryList.add(decimal);
         }
 
-		// 배열에 저장된 10진수 값 중 최대값 찾아 출력
-        for(int i = 0; i < a.length(); i++)
-            res = Math.max(res, arr[i]);
-        System.out.print(res);
+        // 3진수를 1개씩 변경하여 10진수로 변환하여 리스트에 담기
+        for (int i = 0; i < b.length(); i++) {
+            char current = b.charAt(i);
+            String front = b.substring(0, i);
+            String back = b.substring(i + 1);
+
+            for (char j = '0'; j <= '2'; j++) {
+                if (current != j) {
+                    current = j;
+                    int decimal = ternaryToDecimal(front + current + back);
+                    ternaryList.add(decimal);
+                }
+            }
+        }
+
+        for (int i = 0; i < binaryList.size(); i++) {
+            if (ternaryList.contains(binaryList.get(i))) {
+                System.out.println(binaryList.get(i));
+                break;
+            }
+        }
+    }
+
+    // 2진수를 10진수로 변환
+    private static int binaryToDecimal(String binary) {
+        int decimal = 0;
+
+        for (int i = 0; i < binary.length(); i++) {
+            char digit = binary.charAt(i);
+            int index = binary.length() - i - 1;
+            decimal += (digit - '0') * pow(2, index);
+        }
+
+        return decimal;
+    }
+
+    // 3진수를 10진수로 변환
+    private static int ternaryToDecimal(String ternary) {
+        int decimal = 0;
+
+        for (int i = 0; i < ternary.length(); i++) {
+            char digit = ternary.charAt(i);
+            int index = ternary.length() - i - 1;
+            decimal += (digit - '0') * pow(3, index);
+        }
+
+        return decimal;
+    }
+
+    // 거듭제곱 구하기
+    private static int pow(int base, int exponent) {
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+
+        return result;
     }
 }
