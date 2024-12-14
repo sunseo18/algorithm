4adb2cde41c77982b848596cd1ab86a65fdcab2d
JunSeo <78674565+sunseo18@users.noreply.github.com>
2024-12-14 23:27:26 +0900

    Title: 이상한 진수, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241214/이상한 진수/awkward-digits.java
+++ 241214/이상한 진수/awkward-digits.java
@@ -1,44 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int[] arr = new int[a.length()]; // 10진수를 저장할 배열
+        int res = Integer.MIN_VALUE; 
 
+        for(int i = 0; i < a.length(); i++){
+            char c = a.charAt(i);
+            if(c == '1')
+                c = '0';
+            else if(c == '0')
+                c = '1';
 
-    public static void main(String[] args) throws IOException
-    {
-
-        String binary = br.readLine();
-        String thirdary = br.readLine();
-
-
-        int min = (int) Math.pow(2, (binary.length()-1));
-        int max = (int) Math.pow(2, binary.length())  - 1;
-
-        for (int number = min; number < max+1; number++) {
-            String tmp =  Integer.toString(number,3);
-            if (check(tmp, thirdary)) {
-                System.out.println(Integer.parseInt(tmp, 3));
-            }
+			// 비트 반전시킨 이진수 문자열 binary 변수에 저장
+            String binary = a.substring(0, i) + c + a.substring(i+1); 
+            
+            // 2진수 → 10진수로 변환하여 배열에 저장
+            arr[i] = Integer.parseInt(binary, 2);
         }
 
-    }
-
-    public static boolean check(String a, String b) {
-        int diff = 0;
-
-        if (a.length() != b.length() )
-            return false;
-
-        for ( int i = 0; i < a.length(); i++) {
-            if ( a.charAt(i) != b.charAt(i)) diff++;
-            if (diff > 1) 
-                return false;
-        }
-
-        if (( a.length() == b.length() ) && diff == 1)
-            return true;
-        return false;
+		// 배열에 저장된 10진수 값 중 최대값 찾아 출력
+        for(int i = 0; i < a.length(); i++)
+            res = Math.max(res, arr[i]);
+        System.out.print(res);
     }
 }
